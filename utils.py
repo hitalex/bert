f47346c22a7ac650243fef713cb550d3aebc4363
Qingchao Kong <kqingchao@gmail.com>
2018-11-18 21:43:07 +0800

    use index = False to remove auto index rows

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 #coding=utf8
 
+import sys
+
 def generate_train_dev_test_datasets(path, out_dir):
     """ Generate three different datasets for training
     """
@@ -13,10 +15,11 @@ def generate_train_dev_test_datasets(path, out_dir):
     dev, test = train_test_split(test, test_size = 0.5, random_state=13)
 
     print('Saving datasets...')
-    train.to_csv(out_dir + "/train.csv", ',')
-    dev.to_csv(out_dir + "/dev.csv", ',')
-    test.to_csv(out_dir + "/test.csv", ',')
+    train.to_csv(out_dir + "/train.csv", index = False)
+    dev.to_csv(out_dir + "/dev.csv", index = False)
+    test.to_csv(out_dir + "/test.csv", index = False)
 
 if __name__ == '__main__':
-    
-    generate_train_dev_test_datasets('../datasets/wsdm2019-fakenews/train.csv', '../datasets/fakenews/')
+    path = sys.argv[1]
+    out_dir = sys.argv[2]
+    generate_train_dev_test_datasets(path, out_dir)
