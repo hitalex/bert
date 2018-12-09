6d5db58d0742da70e67b66eab1cd298f2394b39d
Qingchao Kong <kqingchao@gmail.com>
2018-12-09 12:50:49 +0800

    add merge pred prob.

--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@
 
 import sys
 
+import numpy as np
+import pandas as pd
+    
+LABEL_LIST = ['unrelated', 'agreed', 'disagreed']
+
 def generate_train_dev_test_datasets(path, out_dir):
     """ Generate three different datasets for training
     """
@@ -157,6 +162,44 @@ def populate_train_dataset(train_path, output_path):
     new_train = new_train.sample(frac = 1) # shuffle all rows
     #import ipdb; ipdb.set_trace()
     new_train.to_csv(output_path, index = False)
+
+def merge_predict_prob(test_id_list, pred_prob_list):
+    ''' 根据test id列表和概率预测结果，得到最终的预测结果
+        其中每个结果的权重都一样
+    Input:
+        test_id_list: test sample的id列表
+        pred_prob_list: [result1, result2, ...]
+        其中，每个result的格式为：key是test id，value为unrelated, agreed, disagreed的概率
+    Output:
+        pred 最终的预测结果
+    '''
+    test_id_list = list(map(str, test_id_list))
+    num_results = len(pred_prob_list)
+    total_result = dict()
+    y_pred = []
+    for test_id in test_id_list:
+        for result in pred_prob_list:
+            total_result[test_id] = np.array([0] * len(LABEL_LIST), float)
+            if test_id in result:
+                total_result[test_id] += result[test_id]
+        
+        if sum(total_result[teset_id]) > 0:
+            total_result[test_id] = total_result[test_id] / sum(total_result[test_id])
+            y_pred.append(LABEL_LIST[np.argmax(total_result[test_id])])
+        else:
+            # 如果没有预测结果，则直接填写unrelated
+            y_pred.append('unrelated')
+        
+    return y_pred
+
+def write_submit_file(test_id_list, pred, output_path):
+    ''' 写入提交文件
+    '''
+    data = dict()
+    data['Id'] = test_id_list
+    data['Category'] = pred
+    data_df = pd.DataFrame(data)
+    data_df.to_csv(output_path, index = False)
 
 if __name__ == '__main__':
     path = sys.argv[1]
